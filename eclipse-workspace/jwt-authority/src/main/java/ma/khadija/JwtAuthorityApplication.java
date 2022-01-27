74c0a6197b3fda7176bd8f9b694ef7f51fe0755c
khadijaezzayany <khadijaezzayany8@gmail.com>
2022-01-27 16:01:04 +0100

    add modeles pk

---
+++ eclipse-workspace/jwt-authority/src/main/java/ma/khadija/JwtAuthorityApplication.java
@@ -0,0 +1,13 @@
+package ma.khadija;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthorityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthorityApplication.class, args);
+	}
+
+}
---
+++ eclipse-workspace/jwt-authority/src/main/java/ma/khadija/models/User.java
@@ -0,0 +1,11 @@
+package ma.khadija.models;
+
+import java.io.Serializable;
+
+// Serializable Make transition class to database more easily
+
+public class User implements Serializable {
+	private Long id;
+	private String userId;
+
+}
